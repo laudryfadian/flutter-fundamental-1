8707d7f15528178806678e579b802e7d97ba4019
laudryfadian <skakmate87@gmail.com>
2022-08-29 11:17:10 +0700

    Import MyImageWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamental_1/basic_widgets/image_widget.dart';
 import 'package:flutter_fundamental_1/basic_widgets/text_widget.dart';
 
 void main() {
@@ -48,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const MyTextWidget(),
+            const MyImageWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
