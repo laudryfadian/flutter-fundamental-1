7d9a48bb21ffe5649432a49514efbf0baabd5a37
laudryfadian <skakmate87@gmail.com>
2022-08-29 13:36:59 +0700

    Update import dialog, textfield, datepicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fundamental_1/basic_widgets/image_widget.dart';
-import 'package:flutter_fundamental_1/basic_widgets/text_widget.dart';
+import 'package:flutter_fundamental_1/basic_widgets/datepicker_widget.dart';
+import 'package:flutter_fundamental_1/basic_widgets/dialog_widget.dart';
+import 'package:flutter_fundamental_1/basic_widgets/textfield_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,73 +31,19 @@ class MyLayout extends StatefulWidget {
 }
 
 class _MyLayoutState extends State<MyLayout> {
-  // Variable/State untuk mengambil tanggal
-  DateTime selectedDate = DateTime.now();
-
-  //  Initial SelectDate FLutter
-  Future<void> _selectDate(BuildContext context) async {
-    // Initial DateTime FIinal Picked
-    final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime(2015, 8),
-        lastDate: DateTime(2101));
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text("${selectedDate.toLocal()}".split(' ')[0]),
-            const SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-              onPressed: () => {
-                _selectDate(context),
-                // ignore: avoid_print
-                print(selectedDate.day + selectedDate.month + selectedDate.year)
-              },
-              child: const Text('Pilih Tanggal'),
-            ),
+          children: const [
+            MyDialogWidget(),
+            MyTextField(),
+            MyDatePicker(),
           ],
         ),
       ),
     );
   }
-}
-
-showAlertDialog(BuildContext context) {
-  // set up the button
-  Widget okButton = TextButton(
-    child: const Text("OK"),
-    onPressed: () {
-      Navigator.pop(context);
-    },
-  );
-
-  // set up the AlertDialog
-  AlertDialog alert = AlertDialog(
-    title: const Text("My title"),
-    content: const Text("This is my message."),
-    actions: [
-      okButton,
-    ],
-  );
-
-  // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
 }
