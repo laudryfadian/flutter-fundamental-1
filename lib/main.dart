15834ce765ca8297c91d3c19b23f4b32b4a70df4
laudryfadian <skakmate87@gmail.com>
2022-08-29 11:54:46 +0700

    Update datepicker

--- lib/main.dart
+++ lib/main.dart
@@ -30,23 +30,44 @@ class MyLayout extends StatefulWidget {
 }
 
 class _MyLayoutState extends State<MyLayout> {
+  // Variable/State untuk mengambil tanggal
+  DateTime selectedDate = DateTime.now();
+
+  //  Initial SelectDate FLutter
+  Future<void> _selectDate(BuildContext context) async {
+    // Initial DateTime FIinal Picked
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        //   child: ElevatedButton(
-        //     child: const Text('Show alert'),
-        //     onPressed: () {
-        //       showAlertDialog(context);
-        //     },
-        //   ),
-        // ),
-        child: TextField(
-          obscureText: false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Nama',
-          ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("${selectedDate.toLocal()}".split(' ')[0]),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () => {
+                _selectDate(context),
+                // ignore: avoid_print
+                print(selectedDate.day + selectedDate.month + selectedDate.year)
+              },
+              child: const Text('Pilih Tanggal'),
+            ),
+          ],
         ),
       ),
     );
